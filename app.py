0225b40e26c35e5eb1c5181bfaf1c60548bc619c
sravaani kunta <kuntasravanipatel@gmail.com>
2026-08-14 18:10:32 +0530

    Add farmer dashboard and login

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -9,7 +9,35 @@ def home():
     <h1>🌾 Farmer Connect</h1>
     <h2>Welcome to Farmer Connect</h2>
     <p>Connecting farmers, dealers and delivery partners.</p>
+
+    <a href="/login">
+        <button>Farmer Login</button>
+    </a>
     """
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+
+    if request.method == "POST":
+
+        phone = request.form["phone"]
+        password = request.form["password"]
+
+        if phone == "9876543210" and password == "farmer123":
+            return render_template("farmer_dashboard.html")
+
+        return """
+        <h1>❌ Invalid Login</h1>
+        <a href="/login">Try Again</a>
+        """
+
+    return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    return redirect("/")
 
 
 if __name__ == "__main__":
